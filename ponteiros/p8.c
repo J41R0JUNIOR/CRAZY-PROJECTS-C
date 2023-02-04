c517c6861494beeadfca346215ef8a6b468c4c2c
J41R0JUNIOR <jairojjr2003@gmail.com>
2023-02-04 15:50:51 -0300

    ++i;

--- ponteiros/p8.c
+++ ponteiros/p8.c
@@ -8,8 +8,9 @@ int main (void){
     //int vetor[3] = {1,2,3};
     //int *ponteiro = &vetor[0];
 
-    //int vetor[3] = {1,2,3};
-    //int *ponteiro = vetor;
+    int vetor[3] = {1,2,3};
+    int *ponteiro = vetor;
+    printf("%i", ++*ponteiro);
 
     return 0;
 }
