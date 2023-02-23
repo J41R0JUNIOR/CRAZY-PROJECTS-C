c0b65b380d2e3f48e08d9ee66ad09547bbf8a063
J41R0JUNIOR <jairojjr2003@gmail.com>
2023-02-22 21:28:47 -0300

    ++sofrimento

---
+++ estruturaDeDados/lista/l1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+typedef struct no{
+    int valor;
+    struct no *proximo;
+}No;
+
+typedef struct {
+    No *inicio;
+    int tam;
+}Lista;
+
+void inserirInicio(Lista *lista, int valor){
+    No *novo = (No*)malloc(sizeof(No));
+    novo->valor = valor;
+    novo->proximo = lista->inicio;
+    lista->inicio = novo;
+    lista->tam++;
+}
+
+void inserirFim(Lista *lista, int valor){
+    No *no, *novo = (No*)malloc(sizeof(No));
+    novo->valor = valor;
+    novo->proximo = NULL;
+
+    if(lista->inicio == NULL){
+        lista->inicio = novo;
+    }else{
+        no = lista->inicio;
+
+        while (no->proximo != NULL){
+            no = no->proximo;
+        }
+        no->proximo = novo; 
+    }
+    lista->tam++;
+}
+
+void imprimir(Lista *lista){
+    No *inicio = lista->inicio;
+    printf("Tamanho da lista: %d\n", lista->tam);
+    while(inicio != NULL){
+        printf("%d ", inicio->valor);
+        inicio = inicio->proximo;
+    }
+    printf("\n\n");
+}
+
+int main (void){
+    Lista lista;
+
+    lista.inicio = NULL;
+    lista.tam = 0;
+
+    int opcao, valor;
+
+    do{
+        printf("1- Inserir no inicio\n2- Inserir no fim\n3- Imprimir\n4- Sair\n");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+                printf("Digite um valor a ser inserido: ");
+                scanf("%d", &valor);
+                inserirInicio(&lista, valor);
+                break;
+            case 2:
+                printf("Digite um valor a ser inserido: ");
+                scanf("%d", &valor);
+                inserirFim(&lista, valor);
+                break;
+            case 3:
+                imprimir(&lista);
+                break;
+            case 4:
+                printf("Saindo...");
+                break;
+            default:
+                printf("Opcao Invalida");
+        }
+    }while(opcao != 4);
+
+    return 0;
+}
---
+++ projetinhosMalucos/cadastro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void){
+
+    int choice = 0;
+    void SingIn ();
+    void logIn ();
+
+    printf("Choose: \n(1) Sing in\n(2) Login\n");
+
+
+    return 0;
+}
+void singIn (){
+    
+}
+
+void logIn (){
+
+}
