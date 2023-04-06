a8b2aa88a5d19e3a4f724e0f20e476e6b852eae5
J41R0JUNIOR <jairojjr2003@gmail.com>
2023-04-06 14:36:19 -0300

    a

--- atividadesDaUCB/Aula05/a3.c
+++ atividadesDaUCB/Aula05/a3.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+/*
+• De graus Celsius(C) para graus Fahrenheit(F): F = (9 ∗ C/5) + 32)
+
+• De graus Fahrenheit (F) para graus Celsius (C): C = (F − 32) ∗ 5/9
+
+• De graus Celsius (C) para graus Kelvin (K): K = C + 273.15
+
+• De graus Kelvin (K) para graus Celsius (C): C = K − 273.15
+
+• De graus Celsius (C) para graus Réaumur (Re): Re = C ∗ 4/5
+
+• De graus Réaumur (Re) para graus Celsius (C): C = Re ∗ 5/4
+
+• De graus Kelvin (K) para graus Rankine (R): R = K ∗ 1.8
+
+• De graus Rankine (R) para graus Kelvin (K): K = R/1.8 
+*/
 
 int main (void){
 
