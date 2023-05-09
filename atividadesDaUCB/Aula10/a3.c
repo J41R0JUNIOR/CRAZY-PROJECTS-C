da525b1cb26506c9b89335441c8a276e905dad5a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-05-09 09:31:36 -0300

    q

--- atividadesDaUCB/Aula10/a3.c
+++ atividadesDaUCB/Aula10/a3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #include <stdio.h>
-#define TAMVET 3
+#define TAMVET 2
 
 struct pessoa{
     char nome[50];
@@ -19,9 +19,8 @@ int main (void){
     float menorMedia = 0;
     int posicaoMP;
     
-
     for(int i  = 0; i < TAMVET; ++i){
-        /*
+        
         printf("Digite o nome do %d aluno: ", i+1);
         scanf("%[^\n]s", &p[i].nome);
         fflush(stdin);
@@ -33,7 +32,7 @@ int main (void){
         printf("\nDigite o curso do %d aluno: ", i+1);
         scanf("%[^\n]s", &p[i].curso);
         fflush(stdin);
-        */
+        
 
         for(int j = 0; j < 3; ++j){
             printf("\nDigite o nota %d do %d aluno: ",j+1, i+1);
@@ -42,15 +41,12 @@ int main (void){
             printf("%f", p[i].nota[j]);
             
         }
-        if(p[i].nota[0] > p[i-1].nota[0]){
-            mairoNota = p[i].nota[0];
-            posicaoN = i;
-            
-        }else{
-            mairoNota = p[i-1].nota[0];
-            posicaoN = i;
+        for(int j = i + 1; j < TAMVET; ++j){
+            if(p[i].nota[0] > p[j].nota[0]){
+                mairoNota = p[i].nota[0];
+                posicaoN = i;   
+            }
         }
-
         p[i].media = (p[i].nota[0] + p[i].nota[1] + p[i].nota[2]) / 3;
         printf("media = %f\n", p[i].media);
 
@@ -69,14 +65,13 @@ int main (void){
             posicaoMP = i;
         }
     }
-    
-/*
+
     for(int j = 0; j < TAMVET; ++j){
         printf("%i - Nome: %s\n", j+1, p[j].nome);
         printf("%i - Matricula: %d\n", j+1, p[j].matricula);
         printf("%i - Curso: %s\n", j+1, p[j].curso);
     }
-    */
+    
    printf("Maior nota da primeira prova: %.1f, aluno %d\n", mairoNota, posicaoN);
    printf("Maior media geral: %.1f, aluno %d\n", maiorMedia, posicaoM);
    printf("Menor media geral: %.1f, aluno %d", menorMedia, posicaoMP);
