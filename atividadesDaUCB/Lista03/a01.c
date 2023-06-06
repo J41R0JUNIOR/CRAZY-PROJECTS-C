ce61cdc2e73a275a72918d3c165d4d9208f9dfd7
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-06-06 19:32:54 -0300

    A

--- atividadesDaUCB/Lista03/a01.c
+++ atividadesDaUCB/Lista03/a01.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct NO{
-    int valor;
-    struct No* proximo;
-};
+struct Restaurante{
+    char nomeRest[50];
+    char enderecoRest[100];
+    float precoMedioRest;
+    char tipoComidaRest[50];
+    struct Restaurante* proximo;
+}typedef Rest;
 
 int main (void){
+    Rest *inicio =(Rest *)malloc(sizeof(Rest));
+    int qtd = 0;
 
+    //inicio da lista
+    inicio->proximo = NULL;
+
+    //adicionando elementso
+    Rest *Elemento =(Rest *)malloc(sizeof(Rest));
+    
 
 
     return 0;
+}
+
+void solicitarDados(){
+    printf("Sobre o Restaurante\n");
+    printf("Nome: ");
+    printf("Endereco: ");
+    printf("Preco Medio: ");
+    printf("Tipo de Comida: ");
+}
+void listarDados(){
+
 }
